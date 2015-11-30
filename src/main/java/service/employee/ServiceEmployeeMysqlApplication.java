cef847c29cec7c8c971f2b817f309d30c24daac0
gman <gmanpark@tmon.co.kr>
2015-11-30 13:43:46 +0900

    [TASK] add .gitignore

---
+++ src/main/java/service/employee/ServiceEmployeeMysqlApplication.java
@@ -0,0 +1,12 @@
+package service.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceEmployeeMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceEmployeeMysqlApplication.class, args);
+    }
+}
