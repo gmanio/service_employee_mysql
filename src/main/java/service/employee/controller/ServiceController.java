139b158588e08a3e409d6ff1588949d945896221
gmanpark <gmanpark@gmanparkui-MacBook-Pro.local>
2015-12-04 22:41:12 +0900

    [TASK] livereload success

---
+++ src/main/java/service/employee/controller/ServiceController.java
@@ -0,0 +1,24 @@
+package service.employee.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by gmanpark on 2015. 12. 4..
+ */
+@Controller
+public class ServiceController {
+
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String index(){
+
+        logger.info("testter");
+
+        return "index";
+    }
+}
