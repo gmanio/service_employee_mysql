39dd07fc052620fc9966b3e779b2aaf3ebb81075
gman <gmanpark@tmon.co.kr>
2015-12-07 17:41:46 +0900

    [TASK] index fileupload before

--- src/main/java/service/employee/controller/ServiceController.java
+++ src/main/java/service/employee/controller/ServiceController.java
@@ -2,9 +2,11 @@ package service.employee.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import service.employee.eight.ListFilter;
 
 /**
  * Created by gmanpark on 2015-12-03.
@@ -16,6 +18,17 @@ public class ServiceController {
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     public String getName(){
         logger.info("test index");
+        return "index";
+    }
+
+
+    @Autowired
+    private ListFilter listFilter;
+
+    @RequestMapping(value="/lst", method = RequestMethod.GET)
+    public String execute(){
+        logger.info("lst");
+        listFilter.streamFilter();
         return "index";
     }
 }
---
+++ src/main/java/service/employee/eight/ListFilter.java
@@ -0,0 +1,19 @@
+package service.employee.eight;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by gmanpark on 2015-12-07.
+ */
+@Component
+public class ListFilter {
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+    public void streamFilter(){
+    }
+}
